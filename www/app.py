e440c613e518330179adb56f839f9468ff5467e0
baoqiang <baoqiang@uzoo.cn>
2016-08-01 21:30:57 +0800

    add app.py

---
+++ www/app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: BaoQiang
+@license: Apache Licence 
+@contact: mailbaoqiang@gmail.com
+@site: http://www.github.com/githubao
+@software: PyCharm
+@file: app.py
+@time: 2016/8/1 21:20
+"""
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+def init(loop):
+    app=web.Application(loop=loop)
+    app.router.add_route("GET","/",index)
+    srv = yield from loop.create_server(app.make_handler(),"127.0.0.1",9000)
+    logging.info("server started at http://127.0.0.1:9000...")
+    return srv
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init(loop))
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
+
