5cc1d7e4d8798d2fb648b837e994dac828986067
baoqiang <baoqiang@uzoo.cn>
2016-08-02 00:03:39 +0800

    add orm and models

---
+++ www/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: BaoQiang
+@license: Apache Licence 
+@contact: mailbaoqiang@gmail.com
+@site: http://www.github.com/githubao
+@software: PyCharm
+@file: __init__.py
+@time: 2016/8/1 23:52
+"""
+
+
+def main():
+    print("do sth")
+
+
+if __name__ == '__main__':
+    main()
+
