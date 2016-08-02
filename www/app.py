80995aea89dfe59469dd5bb1050601ee6d9cc021
baoqiang <baoqiang@uzoo.cn>
2016-08-03 00:39:31 +0800

    i am sleepy

--- www/app.py
+++ www/app.py
@@ -13,24 +13,60 @@
 @time: 2016/8/1 21:20
 """
 
-import logging; logging.basicConfig(level=logging.INFO)
+import logging;
 
-import asyncio,os,json,time
+logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
 from datetime import datetime
+from os import *
 
 from aiohttp import web
 
+import asyncio, os, json, time
 
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+from www.handlers import index
+
+from jinja2 import Environment, FileSystemLoader
+
+import www.orm
+from www.coroweb import add_routes, add_static
+
+
+def init_jinja2(app, **kw):
+    logging.info('init jinja2...')
+    options = dict(
+            autoescape=kw.get("autoescape", True),
+            block_start_string=kw.get('block_start_string', '{%'),
+            block_end_string=kw.get("block_end_string", "%}"),
+            variable_start_string=kw.get("variable_start_string", '{{'),
+            variable_end_string=kw.get("variable_end_string", '}}'),
+            auto_reload=kw.get("auto_relaod", True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "template")
+    logging.info("set jinja2 template path: %s" % path)
+
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get("filters", None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app["__templating__"] = env
+
+
+# def index(request):
+#     return web.Response(body=b'<h1>Awesome</h1>')
 
 @asyncio.coroutine
 def init(loop):
-    app=web.Application(loop=loop)
-    app.router.add_route("GET","/",index)
-    srv = yield from loop.create_server(app.make_handler(),"127.0.0.1",9000)
+    app = web.Application(loop=loop)
+    app.router.add_route("GET", "/", index)
+    srv = yield from loop.create_server(app.make_handler(), "127.0.0.1", 9000)
     logging.info("server started at http://127.0.0.1:9000...")
     return srv
+
 
 def main():
     loop = asyncio.get_event_loop()
@@ -40,4 +76,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
---
+++ www/handlers.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: BaoQiang
+@license: Apache Licence 
+@contact: mailbaoqiang@gmail.com
+@site: http://www.github.com/githubao
+@software: PyCharm
+@file: handlers.py
+@time: 2016/8/3 0:04
+"""
+
+import re, time, json, logging, hashlib, base64, asyncio
+
+from www.coroweb import get, post
+
+from www.models import User, Comment, Blog, next_id
+
+
+@get("/")
+def index(request):
+    users = yield from User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
+
+
+def main():
+    print("do sth")
+
+
+if __name__ == '__main__':
+    main()
